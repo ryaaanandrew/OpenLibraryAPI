b488dd69e56364647266682670389d7e80f4d13d
Ryan Andrew <ryanandrew.sibat@gmail.com>
2022-07-20 17:16:26 -0400

    udpated snapshots, removed unused text

--- src/Pages/SearchBook/__tests__/SearchBook.spec.tsx
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe("Plan admin company plan view", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000/");
-  });
-
-  it("Plan Admin Dashboard should render with all stats", () => {});
-});
